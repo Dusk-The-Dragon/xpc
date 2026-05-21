4fb585b7e19c5bb526e35c58eec1096c80a0a927
Dusk-The-Dragon <dusklunaris@gmail.com>
2026-05-21 00:56:05 +0000

    Refactor ExpressionNode to use unique_ptr for children instead of vector

--- XPC/xpcparser.h
+++ XPC/xpcparser.h
@@ -15,7 +15,7 @@ struct AtomNode : public ASTNode
 struct ExpressionNode : public ASTNode
 {
     std::string type;
-    std::vector<ASTNode> children;
+    std::unique_ptr<ASTNode> children;
     ExpressionNode(std::string t, std::vector<ASTNode> c) : type(t), children(std::move(c)) {}
 };
 #endif // XPCPARSER_H
